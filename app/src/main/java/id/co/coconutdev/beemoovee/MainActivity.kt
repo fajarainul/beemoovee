825841d76f6c3ba8069b9ccfe8400b0bdf886414
fajar ainul <fajar.bashri@gmail.com>
2021-05-11 12:29:39 +0700

    create fragments (home &favorite)

--- app/src/main/java/id/co/coconutdev/beemoovee/MainActivity.kt
+++ app/src/main/java/id/co/coconutdev/beemoovee/MainActivity.kt
@@ -2,10 +2,36 @@ package id.co.coconutdev.beemoovee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import id.co.coconutdev.beemoovee.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        val homeFragment=HomeFragment()
+        val favoriteFragment=FavoriteFragment()
+
+        setCurrentFragment(homeFragment)
+
+        activityMainBinding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->setCurrentFragment(homeFragment)
+                R.id.menu_favorite->setCurrentFragment(favoriteFragment)
+
+            }
+            true
+        }
+
     }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.framelayout,fragment)
+            commit()
+        }
 }
