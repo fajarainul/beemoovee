d2d419e481c30822a95e4f299e96600a5adc5e5f
fajar ainul <fajar.bashri@gmail.com>
2021-05-14 00:16:11 +0700

    show dummy data movies

---
+++ app/src/main/java/id/co/coconutdev/beemoovee/ui/movies/MovieAdapter.kt
@@ -0,0 +1,47 @@
+package id.co.coconutdev.beemoovee.ui.movies
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import id.co.coconutdev.beemoovee.data.Movie
+import id.co.coconutdev.beemoovee.databinding.ItemMovieBinding
+
+class MovieAdapter: RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+    private var listMovies = ArrayList<Movie>()
+
+    fun setMovies(movies:List<Movie>?){
+        if (movies == null) return
+        this.listMovies.clear()
+        this.listMovies.addAll(movies)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieAdapter.MovieViewHolder {
+        val itemsAcademyBinding = ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MovieViewHolder(itemsAcademyBinding)
+    }
+
+    override fun onBindViewHolder(holder: MovieAdapter.MovieViewHolder, position: Int) {
+        val movie = listMovies[position]
+        holder.bind(movie)
+    }
+
+    override fun getItemCount(): Int {
+        return  listMovies.size
+    }
+
+    class MovieViewHolder (private val binding: ItemMovieBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(movie: Movie) {
+            with(binding) {
+                titleMovie.text = movie.title
+                releaseDateMovie.text = movie.releaseDate
+                ratingMovie.text = movie.rating.toString()
+
+                val imageRes = itemView.context.resources.getIdentifier(movie.imageUrl, "drawable", itemView.context.packageName)
+                Glide.with(itemView.context)
+                        .load(imageRes)
+                        .into(imagePosterMovie)
+            }
+        }
+    }
+}
--- app/src/main/java/id/co/coconutdev/beemoovee/ui/movies/MoviesFragment.kt
+++ app/src/main/java/id/co/coconutdev/beemoovee/ui/movies/MoviesFragment.kt
@@ -5,20 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.coconutdev.beemoovee.R
+import id.co.coconutdev.beemoovee.databinding.FragmentMoviesBinding
+import id.co.coconutdev.beemoovee.utils.DummyData
 
 class MoviesFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var fragmentMovieBinding:FragmentMoviesBinding
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        fragmentMovieBinding = FragmentMoviesBinding.inflate(inflater, container, false);
+        return fragmentMovieBinding.root;
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if(activity != null){
+            val movies = DummyData.generateDummyMovies()
+            val movieAdapter = MovieAdapter();
+
+            movieAdapter.setMovies(movies)
+            with(fragmentMovieBinding.recyclerviewMovies){
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                adapter = movieAdapter
+            }
+        }
     }
 }
