477c7471b80f0f929d6aabbdce6582c66e70ad01
fajar ainul <fajar.bashri@gmail.com>
2021-05-13 23:41:22 +0700

    create dummy data

---
+++ app/src/main/java/id/co/coconutdev/beemoovee/data/TvShow.kt
@@ -0,0 +1,9 @@
+package id.co.coconutdev.beemoovee.data
+
+data class TvShow(
+    val title:String,
+    val releaseDate:String,
+    val rating:Double,
+    val imageUrl:String,
+    val description:String,
+)
